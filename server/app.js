0065568502be72bb5ee6e69a8715093d96ebeadf
shamuvel <shamu7757@gmail.com>
2025-10-19 16:49:14 +0530

    adding some changes on server.js and app.js files

---
+++ server/app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import { config } from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+
+const app = express();
+
+config({ path: "./config/config.env" });
+
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  fileUpload({
+    tempFileDir: "./uploads",
+    useTempFiles: true,
+  })
+);
+
+export default app;
